87e966cf1e8d9e8d4c888bbeeada4331dad3cd69
medegor44 <medegor44@gmail.com>
2015-07-19 22:13:44 +0300

    Добавлена генерация лабиринта с несколькими путями и исправлены ошибки.

--- maze_generator/generator.h
+++ maze_generator/generator.h
@@ -6,13 +6,16 @@
 #include <QStack>
 #include <QTextStream>
 #include <QFile>
+#include <cmath>
 
 #include <random>
 #include <chrono>
+#include <iostream>
 
-#define FREE_CELL 0
-#define WALL 1
+#define FREE_CELL 1
+#define WALL 0
 #define VISITED 2
+#define elif(cond) else if((cond))
 
 class Generator
 {
@@ -21,10 +24,13 @@ private:
     int height, cellsHeight;
     int cellCount;
 
+    const double ratio = 1.0 / 5.0;
+
     int **maze;
 
     void initMatrix();
     // Инициализация матрицы, на которой будет создаваться лабиринт
+    void makeIndefinite();
 
     QVector <QPoint> getNeighbours(QPoint cell);
     // Получение соседей клетки с координатами cell
@@ -32,7 +38,7 @@ private:
 public:
     Generator(int w, int h);
 
-    void start();
+    void start(bool indefinite = false);
     void print();
 };
 
